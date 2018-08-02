70ac1f8305942646049cd87ec6a008573eb11ba0
abudahsh <willcodahsh@gmail.com>
2018-08-02 03:55:20 +0200

    fixing bug

--- tweeter/wsgi.py
+++ tweeter/wsgi.py
@@ -13,5 +13,5 @@ from django.core.wsgi import get_wsgi_application
 from whitenoise.django import DjangoWhiteNoise
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'tweeter.settings')
-application = DjangoWhiteNoise(application)
 application = get_wsgi_application()
+application = DjangoWhiteNoise(application)
